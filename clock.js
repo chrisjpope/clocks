6da35dbdabcf4c3f2bb728d226845f1fbc7072f9
chrisjpope <christopher.james.pope@googlemail.com>
2020-12-01 22:45:55 +0000

    Update clock.js

--- clock.js
+++ clock.js
@@ -2,7 +2,7 @@ function init() {
    for (var i = 0; i < 60; i++) {
       $(".dots").append('<div class="dot" style="transform:translate(-50%,-50%) rotate(' + (i * 6) + 'deg)"></div>');
       /*Haven't looked into html pre-processors*/
-   }
+   };
    var d = new Date();
    var hour = ((d.getHours()%12) * 60 + d.getMinutes()) * 60 + d.getSeconds(); /*hours + minutes + seconds value in seconds*/
    var minute = d.getMinutes() * 60 + d.getSeconds(); /*minutes + seconds value in seconds*/
@@ -11,5 +11,5 @@ function init() {
    $(".minute, .minute >.hand").css("animation-delay","-" + minute + "s");
    $(".second, .second >.hand").css("animation-delay","-" + second + "s");
    
-}
+};
 init();
