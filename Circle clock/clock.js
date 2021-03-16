639ff25658e64a9d66b6a346ef21e07855b70370
chrisjpope <christopher.james.pope@googlemail.com>
2021-03-16 23:10:17 +0000

    Create clock.js

---
+++ Circle clock/clock.js
@@ -0,0 +1,6 @@
+const svg = document.querySelector('svg');
+const currentTime = new Date();
+
+svg.style.setProperty('--start-seconds', currentTime.getSeconds());
+svg.style.setProperty('--start-minutes', currentTime.getMinutes());
+svg.style.setProperty('--start-hours', currentTime.getHours() % 12);
